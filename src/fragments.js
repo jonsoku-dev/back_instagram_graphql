2239d980df389e3b3f90f5fc0b09d1b512e27618
rakutenjongseok <whdtjr2792@gmail.com>
2019-09-20 20:27:19 +0900

    #3.19 seeFullPost Refactoring~

--- src/fragments.js
+++ src/fragments.js
@@ -1,9 +1,35 @@
+// data 모델에 있는 이름과 같아야한다 : User
+export const USER_FRAGMENT = `
+    id
+    username
+`;
 export const COMMENT_FRAGMENT = `
-    fragment CommentParts on Comment{
+    id
+    text
+    user {
         id
-        text
-        user {
-            username
-        }
+        username
     }
-    `;
+`;
+
+export const FILE_FRAGMENT = `
+    id
+    url
+`;
+
+export const FULL_POST_FRAGMENT = `
+fragment PostParts on Post{
+    id
+    location
+    caption
+    files {
+        ${FILE_FRAGMENT}
+    }
+    comments { 
+        ${COMMENT_FRAGMENT}
+    }
+    user {
+        ${USER_FRAGMENT}
+    }
+}
+`;
